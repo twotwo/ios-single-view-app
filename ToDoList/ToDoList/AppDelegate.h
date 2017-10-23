90a4f5b1f8cd70b39c8da8a052c86b89466673fc
twotwo <twotwo.li@gmail.com>
2017-10-23 18:59:06 +0800

    初试提交： 1. Doc Doc/README.md Doc/Start-Developing-iOS-Apps-Today-non-IB.pdf 2. ToDoList 新建的iOS Single View Application

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by liyan on 23/10/2017.
+//  Copyright © 2017 liyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
